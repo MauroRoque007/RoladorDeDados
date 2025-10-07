580e1ef3f4580dc790cefd07f034ba6a801f31dd
Mauro Roque <mauroalexandresilvaroque@gmail.com>
2025-10-06 22:24:19 -0300

    Começo da programação orientada a objeto

---
+++ RoladorDados/Models/Dado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoladorDados.Models
+{
+    public class Dado
+    {
+        private int qteLados;
+        private int ladoSorteado;
+        
+        public Dado(int lados)
+        {
+            qteLados = lados;
+        }
+
+        public void Rolar()
+        {
+            ladoSorteado = Random.Shared.Next(1, qteLados + 1);
+        }
+    }
+}
